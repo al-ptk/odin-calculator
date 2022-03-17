76a3f60480e54f1b39fc3cd62907c2e9994ed689
Alan Patrick <alan.patrick.fv@gmail.com>
2022-03-17 17:49:13 -0300

    Add a second stylesheet object

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ function bindButtonsToArea (calc) {
     }
 }
 
-const stylesheet = {
+const stylesheet1 = {
+    'display' : {
+        'background':       'white',
+        'color':            '#2f2f2f',
+    },
     'number' : {
         'background':       'lightgreen',
         'border':           '.1rem solid',
@@ -17,7 +21,7 @@ const stylesheet = {
     },
     'operator' : {
         'background':       'lightblue',
-        'border':           '.1rem solid',
+        'border':           '.1rem solid black',
         'border-radius':    '.5rem',
         'color':            'black'
     },
@@ -29,17 +33,41 @@ const stylesheet = {
     }
 }
 
-function applyStyles(calc) {
+const stylesheet2 = {
+    'display' : {
+        'background':       'black',
+        'color':            'rgb(0, 186, 248)',
+    },
+    'number' : {
+        'background':       'black',
+        'border':           '.2rem solid',
+        'border-radius':    '.5rem',
+        'color':            'lightgreen'
+    },
+    'operator' : {
+        'background':       'black',
+        'border':           '.2rem solid',
+        'border-radius':    '.5rem',
+        'color':            'lightblue'
+    },
+    'function' : {
+        'background':       'black',
+        'border':           '.2rem solid',
+        'border-radius':    '.5rem',
+        'color':            'lightsalmon'
+    }
+}
+
+function applyStyles(parent, stylesheet) {
     const pickStyleBy = (className) => stylesheet[className];
-    const btns = calc.querySelectorAll('button');
-    for (const btn of btns){
-        const btnProperties = pickStyleBy(btn.className);
-        for (const styleProperty in btnProperties){
-            btn.style.setProperty(styleProperty, btnProperties[styleProperty]);
+    for (const elem of parent.children){
+        const elemProperties = pickStyleBy(elem.className);
+        for (const styleProperty in elemProperties){
+            elem.style.setProperty(styleProperty, elemProperties[styleProperty]);
         }
     }
 }
 
 const calc = document.querySelector('.calculator');
 bindButtonsToArea(calc);
-applyStyles(calc);
+applyStyles(calc, stylesheet1);
