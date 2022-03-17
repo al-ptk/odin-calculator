456319868db91aa9695edcc5b26795938c9f194b
Alan Patrick <alan.patrick.fv@gmail.com>
2022-03-17 17:10:30 -0300

    Define applyStyles function
        • Match a DOM element's class with stylesheet sub-object of same
        name; set the rules from the object to the element's properties.

--- script.js
+++ script.js
@@ -5,9 +5,41 @@ function bindButtonsToArea (calc) {
     const btns = calc.children;
     for (const btn of btns) {
         btn.style.gridArea = btn.id;
-        p(btn.style.gridArea);
+    }
+}
+
+const stylesheet = {
+    'number' : {
+        'background':       'lightgreen',
+        'border':           '.1rem solid',
+        'border-radius':    '.5rem',
+        'color':            'black'
+    },
+    'operator' : {
+        'background':       'lightblue',
+        'border':           '.1rem solid',
+        'border-radius':    '.5rem',
+        'color':            'black'
+    },
+    'function' : {
+        'background':       'lightsalmon',
+        'border':           '.1rem solid',
+        'border-radius':    '.5rem',
+        'color':            'black'
+    }
+}
+
+function applyStyles(calc) {
+    const pickStyleBy = (className) => stylesheet[className];
+    const btns = calc.querySelectorAll('button');
+    for (const btn of btns){
+        const btnProperties = pickStyleBy(btn.className);
+        for (const styleProperty in btnProperties){
+            btn.style.setProperty(btnProperties, btnProperties[styleProperty]);
+        }
     }
 }
 
 const calc = document.querySelector('.calculator');
 bindButtonsToArea(calc);
+configureButtonStyles(calc);
