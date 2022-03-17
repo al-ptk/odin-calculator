91db990d9e1555137d2b895a63e1ba7746219935
Alan Patrick <alan.patrick.fv@gmail.com>
2022-03-17 15:52:45 -0300

    Arrange calculator button
        • Loop through calculator children and bind them to grid area
        using their id name.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
 const p = (str) => console.log(str);
+
+function bindButtonsToArea (calc) {
+    const btns = calc.children;
+    for (const btn of btns) {
+        btn.style.gridArea = btn.id;
+        p(btn.style.gridArea);
+    }
+}
+
+const calc = document.querySelector('.calculator');
+bindButtonsToArea(calc);
